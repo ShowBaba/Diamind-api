108aea5e7c91cfdc9bb0603f658327d413faab1e
ShowBaba <samwise858@gmail.com>
2020-11-03 15:40:59 +0100

    config for deployment

--- db/config/config.js
+++ db/config/config.js
@@ -18,6 +18,13 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: 'DATABASE_URL'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
